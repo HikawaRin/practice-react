85d1bf0c17754dd761f2823f31563555c910cd34
Hikawa Rin <releaseavalon@gmail.com>
2022-11-01 09:01:07 +0000

    Navbar & Styling

--- react_info_site/src/components/Navbar.js
+++ react_info_site/src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import logo from '../logo.svg'
 
 function Navbar() { 
     return (
-        <h1>Navbar</h1>
+        <nav>
+            <img src={logo} alt="logo" className='nav--icon'/>
+            <h3 className='nav--logo_text'>ReactFacts</h3>
+            <h4 className='nav--title'>React Course - Project 1</h4>
+        </nav>
     );
 }
 
