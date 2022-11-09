696f223299aa68961fa3713976b3f13ad7794a3e
Hikawa Rin <releaseavalon@gmail.com>
2022-11-09 11:05:30 +0000

    Introduce React State

--- meme_generator/src/components/Meme.js
+++ meme_generator/src/components/Meme.js
@@ -1,23 +1,34 @@
+import React from 'react';
 import './Meme.css';
 import memesData from "../memesData.js";
 
 const response = memesData();
 
 function Meme() { 
-    function getMeme() { 
+    const [meme, UpdateMeme] = React.useState({
+        'topText': '',
+        'bottomText': '',
+        'randomImage': response.data.memes[0].url,
+    });
+
+    let getMeme = () => { 
         let index = Math.floor(Math.random() * response.data.memes.length);
-        console.log(response.data.memes[index]) 
-    } 
+        UpdateMeme((prevMeme) => {
+            return ({ ...prevMeme, randomImage : response.data.memes[index].url });
+        });
+    };
 
     return (
-        <div className="Meme">
-            <input id='Meme-upper' type='text' value='Shut up'/>
-            <input id='Meme-lower' type='text' value='and take my money'/>
-            <button className="Meme-btn" type="button"
-                onClick={getMeme}>
-                Get a new meme image 🖼
-            </button>
-                    
+        <div className='Meme'>
+            <div className='Meme-form'>            
+                <input id='Meme-upper' type='text' value='Shut up'/>
+                <input id='Meme-lower' type='text' value='and take my money'/>
+                <button className="Meme-btn" type="button"
+                    onClick={getMeme}>
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img className='Meme-img' src={meme.randomImage} alt='Meme-pic'/>                    
         </div>
     );
 }
