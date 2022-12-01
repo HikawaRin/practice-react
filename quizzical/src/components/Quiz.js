5cb38881044fbd3adbdaaff1a218a0a0462213a7
Hikawa Rin <HikawaRin@outlook.com>
2022-12-01 18:49:40 +0800

    handle React useEffect Initial two times problem

--- quizzical/src/components/Quiz.js
+++ quizzical/src/components/Quiz.js
@@ -35,7 +35,8 @@ function Quiz() {
         setLoading(false);
     };
 
-    React.useEffect(() => {
+    useEffectOnce(() => {
+        console.log("Initial render");
         (async () => await NewGame())();
     }, []);
 
@@ -144,5 +145,36 @@ function Choice(props) {
         </div>
     );
 }
+
+const useEffectOnce = ( effect )=> {
+
+  const destroyFunc = React.useRef();
+  const effectCalled = React.useRef(false);
+  const renderAfterCalled = React.useRef(false);
+  const [val, setVal] = React.useState(0);
+
+  if (effectCalled.current) {
+      renderAfterCalled.current = true;
+  }
+
+  React.useEffect( ()=> {
+
+      // only execute the effect first time around
+      if (!effectCalled.current) { 
+        destroyFunc.current = effect();
+        effectCalled.current = true;
+      }
+
+      // this forces one render after the effect is run
+      setVal(val => val + 1);
+
+      return ()=> {
+        // if the comp didn't render since the useEffect was called,
+        // we know it's the dummy React cycle
+        if (!renderAfterCalled.current) { return; }
+        if (destroyFunc.current) { destroyFunc.current(); }
+      };
+  }, []);
+};
 
 export default Quiz;
